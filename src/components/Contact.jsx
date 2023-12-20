c6de73e8b1602b62c3a2dcaba57ae29d7f7f0b32
Betelhemmesele <betimesele93@gmail.com>
2023-12-20 16:58:18 +0300

    update contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -24,7 +24,7 @@ function handleSubmit(e) {
 }
 
   return (
-  <section id="contact" className="relative px-80">
+  <section id="contact" className=" fixed  bg-gray-900 bg-cover h-screen px-80 bg-grey-900">
       <div className=" items-center px-40 py-10 mx-auto flex sm:flex-nowrap flex-wrap justify-center">
         {/* Added 'justify-center' class to center the form */}
         <form
@@ -35,7 +35,7 @@ function handleSubmit(e) {
           <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
             Hire Me
           </h2>
-          <p className="leading-relaxed mb-5">
+          <p className="leading-relaxed text-white mb-5">
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum
             suscipit officia aspernatur veritatis. Asperiores, aliquid?
           </p>
@@ -48,7 +48,7 @@ function handleSubmit(e) {
               id="name"
               name="name"
               className="w-full bg-gray-300 rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none
-               text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+               text-gray-900 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                onChange={(e) => setName(e.target.value)}
             />
           </div>
@@ -61,7 +61,7 @@ function handleSubmit(e) {
               id="email"
               name="email"
               className="w-full bg-gray-300 rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none
-               text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+               text-gray-900 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                onChange={(e) => setEmail(e.target.value)}
             />
           </div>
@@ -74,13 +74,14 @@ function handleSubmit(e) {
             <textarea
               id="message"
               name="message"
-              className="w-full bg-gray-300 rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+              className="w-full bg-gray-300 rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none
+               text-gray-900 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
               onChange={(e) => setMessage(e.target.value)}
             />
           </div>
           <button
             type="submit"
-            className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+            className="text-black bg-white border-0 py-2 px-6 focus:outline-none hover:bg-indigo-100 rounded text-lg">
             Submit
           </button>
         </form>
