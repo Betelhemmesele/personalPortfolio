14d8c1ca8d1b8676e97c3236442d7f45d51f25ef
Betelhemmesele <betimesele93@gmail.com>
2024-07-14 18:56:31 +0300

    fix

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -27,14 +27,14 @@ export default function Skills() {
             </div>
           ))}
         </div>
-        <div className="text-center mb-20">
+        {/* <div className="text-center mb-20">
         <h1 className="sm:text-4xl text-3xl font-medium title-font text-white mb-4">
             Certification
           </h1>
           <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">
             The certification i have received ...
           </p>
-        </div>
+        </div> */}
         {/* <div>
         <Slider />
       </div> */}
